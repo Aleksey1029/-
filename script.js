cffaad8cad6765b2b5ed02661e05d23258eec4bd
Alex2910 <alexlll12345lll@gmail.com>
2024-01-24 18:42:38 +0500

    gotovo

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ let password2 = document.querySelectorAll('.password')[1]
 
 let userNameReg = /^[A-Za-zА-Яа-я\- ]*[^0-9][A-Za-zА-Яа-я\- ]*$/
 let userEmailReg = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/
-let userPasswordReg =
-	/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/
+let userPasswordReg = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/
 
 let btn = document.querySelector('.btn')
 
